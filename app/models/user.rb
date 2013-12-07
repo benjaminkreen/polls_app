f02f0ae6e839fdd250a362f042fe89aefab1bf8d
App Academy Student <student@appacademy.io>
2013-12-06 18:09:50 -0800

    added framework for User#completed_polls

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,9 +17,49 @@ class User < ActiveRecord::Base
   )
 
   def completed_polls
-    Polls.select("polls.*, COUNT(response.user_id) tally")
-    .joins(:question => :response).where('response.id = ?', self.id)
+    # Polls.select("polls.*, COUNT(response.user_id) tally")
+    # .joins(:question => :response).where('response.id = ?', self.id)
+    # .group("Polls.id")
+
+    tally = Poll.find_by_sql([<<-SQL, self.id])
+    SELECT
+  polls.*, COUNT(questions.id) question_count, sub.user_response_count user_count
+  FROM
+  polls
+  JOIN
+  questions
+  ON
+  polls.id = questions.poll_id
+  JOIN
+  (SELECT
+  polls.id id, polls.title AS title , COUNT(responses.user_id) AS user_response_count
+    FROM
+    polls
+    JOIN
+    questions
+    ON
+    polls.id = questions.poll_id
+    JOIN
+    answer_choices
+    ON
+    questions.id = answer_choices.question_id
+    JOIN
+    responses
+    ON
+    answer_choices.id = responses.answer_choice_id
+    WHERE
+    responses.user_id = ?
+    GROUP BY
+  polls.id) AS sub
+  ON
+  polls.id = sub.id
+    SQL
+    output = []
+
+    p tally
+    # tally.each do |poll_data|
+   #    output << [poll_data.name, poll_data.question_count, poll_data.user_count]
+   #  end
+   #  output
   end
-
-
 end
